0f87614df5bc54666948a47ee739c1fe09bd8acd
Scheila Lumi <scheilalumi@hotmail.com>
2020-12-03 00:12:47 -0300

    Bad solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -5,13 +5,15 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Reservation {
-	
+
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
 
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
-	// está sendo declarado como Static para que não seja declarado um novo sdf para cada objeto reservation que aplicação tiver 
+
+	// está sendo declarado como Static para que não seja declarado um novo sdf para
+	// cada objeto reservation que aplicação tiver
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
@@ -33,27 +35,29 @@ public class Reservation {
 	public Date getCheckOut() {
 		return checkOut;
 	}
-	
+
 	public long duration() {
-		long diff = checkOut.getTime() - checkIn.getTime(); //diferença em milisegundos
-		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); //para converter milisegundos para dias
+		long diff = checkOut.getTime() - checkIn.getTime(); // diferença em milisegundos
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); // para converter milisegundos para dias
 	}
-		
-	public void updateDates(Date checkIn, Date checkOut) {
+
+	public String updateDates(Date checkIn, Date checkOut) {
+
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Reservation dates for update must be future dates";
+		}
+		if (!checkOut.after(checkIn)) {
+			return "Check-out must be after check-in date";
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null; //se retornar nulo é pq não deu nenhum erro
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Room "
-				+ roomNumber
-				+ ", check-in: "
-				+ sdf.format(checkIn) 
-				+ ", check-out: "
-				+ sdf.format(checkOut)
-				+ ", "
-				+ duration()
-				+ " nights";
+		return "Room " + roomNumber + ", check-in: " + sdf.format(checkIn) + ", check-out: " + sdf.format(checkOut)
+				+ ", " + duration() + " nights";
 	}
 }
